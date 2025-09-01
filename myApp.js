ec557884ab8d7fd5a8e31b9da1e87dea6c531869
YokPhanu <162823222+phanuwat1144@users.noreply.github.com>
2025-09-01 18:49:11 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// เชื่อม MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -17,121 +16,15 @@ const personSchema = new mongoose.Schema({
 // Model
 const Person = mongoose.model("Person", personSchema);
 
-// 1) Create and Save One Person
-const createAndSavePerson = (done) => {
-  const person = new Person({
-    name: "John",
-    age: 25,
-    favoriteFoods: ["pizza", "burger"]
-  });
-
-  person.save((err, data) => {
-    if (err) return done(err);
-    return done(null, data);
-  });
-};
-
-// 2) Create Many Records
-const createManyPeople = (arrayOfPeople, done) => {
-  Person.create(arrayOfPeople, (err, people) => {
-    if (err) return done(err);
-    return done(null, people);
-  });
-};
-
-// 3) Find People by Name
-const findPeopleByName = (personName, done) => {
-  Person.find({ name: personName }, (err, people) => {
-    if (err) return done(err);
-    return done(null, people);
-  });
-};
-
-// 4) Find One by Food
-const findOneByFood = (food, done) => {
-  Person.findOne({ favoriteFoods: food }, (err, person) => {
-    if (err) return done(err);
-    return done(null, person);
-  });
-};
-
-// 5) Find by ID
-const findPersonById = (personId, done) => {
-  Person.findById(personId, (err, person) => {
-    if (err) return done(err);
-    return done(null, person);
-  });
-};
-
-// 6) Find, Edit then Save
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
-
-  Person.findById(personId, (err, person) => {
-    if (err) return done(err);
-    person.favoriteFoods.push(foodToAdd);
-    person.save((err, updatedPerson) => {
-      if (err) return done(err);
-      return done(null, updatedPerson);
-    });
-  });
-};
-
-// 7) Find One and Update
-const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
-
-  Person.findOneAndUpdate(
-    { name: personName },
-    { age: ageToSet },
-    { new: true },
-    (err, updatedDoc) => {
-      if (err) return done(err);
-      return done(null, updatedDoc);
-    }
-  );
-};
-
-// 8) Delete One by ID
-const removeById = (personId, done) => {
-  Person.findByIdAndRemove(personId, (err, removedDoc) => {
-    if (err) return done(err);
-    return done(null, removedDoc);
-  });
-};
-
-// 9) Delete Many People (FCC Hint)
+// 🔹 Remove Many People
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  Person.remove({ name: nameToRemove }, (err, data) => {
+  Person.remove({ name: nameToRemove }, (err, result) => {
     if (err) return done(err);
-    done(null, data);
+    return done(null, result);
   });
 };
-// 10) Chain Search Query Helpers
-const queryChain = (done) => {
-  const foodToSearch = "burrito";
 
-  Person.find({ favoriteFoods: foodToSearch })
-    .sort({ name: 1 })
-    .limit(2)
-    .select("-age")
-    .exec((err, data) => {
-      if (err) return done(err);
-      return done(null, data);
-    });
-};
-
-// Export
+// 🔹 Export ให้ FreeCodeCamp ใช้
 exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
-exports.createManyPeople = createManyPeople;
-exports.findPeopleByName = findPeopleByName;
-exports.findOneByFood = findOneByFood;
-exports.findPersonById = findPersonById;
-exports.findEditThenSave = findEditThenSave;
-exports.findAndUpdate = findAndUpdate;
-exports.removeById = removeById;
 exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
